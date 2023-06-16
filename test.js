5c7c8db8e1965131d19faf53431583e0ee18d979
DairenDaingah <s21810017@student.unklab.ac.id>
2023-06-16 09:46:08 +0800

    adding test.js

---
+++ test.js
@@ -0,0 +1,66 @@
+//1.
+// let programming = {
+//     languages: ["JavaScript", "Python", "Ruby"],
+//     isChallenging: true,
+//     isRewarding: true,
+//     difficulty: 8,
+//     jokes: "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke"
+// };
+
+// programming.languages.push("Go");
+
+// console.log(programming.languages);
+
+//2.
+// let programming = {
+//     languages: ["JavaScript", "Python", "Ruby"],
+//     isChallenging: true,
+//     isRewarding: true,
+//     difficulty: 8,
+//     jokes: "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke"
+// };
+
+// programming["difficulty"] = 7;
+
+// console.log(programming.difficulty);
+
+//3.
+// let programming = {
+//     languages: ["JavaScript", "Python", "Ruby"],
+//     isChallenging: true,
+//     isRewarding: true,
+//     difficulty: 8,
+//     jokes: "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke"
+// };
+
+// delete programming.jokes;
+
+// console.log(programming);
+
+//4.
+// let programming = {
+//     languages: ["JavaScript", "Python", "Ruby"],
+//     isChallenging: true,
+//     isRewarding: true,
+//     difficulty: 8,
+//     jokes: "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke"
+// };
+
+// programming.isFun = true;
+
+// console.log(programming);
+
+//5.
+// let programming = {
+//     languages: ["JavaScript", "Python", "Ruby"],
+//     isChallenging: true,
+//     isRewarding: true,
+//     difficulty: 8,
+//     jokes: "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke"
+// };
+
+// let updatedLanguages = programming.languages.map((language, index) => {
+//     return `${index} - ${language}`;
+// });
+
+// console.log(updatedLanguages);
