8dd6c94534a16edb792881ce982612260e960928
riderkaunang <riderkaunang@gmail.com>
2023-06-20 20:26:11 +0800

    adding SearchBar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import MyButton from './component/MyButton';
 import Navbar from './component/Navbar';
 import Footer from './component/Footer';
+import SearchBar from './component/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <SearchBar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          Halo ini yang mo pull Kelompok Oslo cuy
---
+++ src/component/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    onSearch(searchTerm);
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        placeholder="Cari..."
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchBar;
