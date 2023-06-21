a65651cde5bb676a60620775fac10153e8ed9f0a
DairenDaingah <s21810017@student.unklab.ac.id>
2023-06-21 08:47:54 +0800

    adding file

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import MyButton from './component/MyButton';
 import Navbar from './component/Navbar';
 import Footer from './component/Footer';
 import SearchBar from './component/SearchBar';
+import Home from './component/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <Home />
         <SearchBar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
