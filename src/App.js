ca01c6dec594b74d5ab19ef1b5a066f04f682683
stevemandias <mandiasjr02@gmail.com>
2023-06-28 00:02:20 +0900

    adding section1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Section1 from './components/Section1';
 
 function App() {
   return (
---
+++ src/components/Section1.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Section1() {
+  return (
+    <div><section className="section main-banner" id="top" data-section="section1">
+    <video autoPlay muted loop id="bg-video">
+      <source src="assets/images/course-video.mp4" type="video/mp4" />
+    </video>
+    <div className="video-overlay header-text">
+      <div className="caption">
+        <h6>Graduate School of Management</h6>
+        <h2><em>Your</em> Classroom</h2>
+        <div className="main-button">
+          <div className="scroll-to-section"><a href="#section2">Discover more</a></div>
+        </div>
+      </div>
+    </div>
+  </section></div>
+  )
+}
+
+export default Section1
