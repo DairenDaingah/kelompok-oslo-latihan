1a41db4627c6e3655b6b723fa059b775bc5af789
Santi <janitraprimasanti06@gmail.com>
2023-06-27 22:37:04 +0700

    adding Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Section1 from './components/Section1';
 import Section2 from './components/Section2';
+import Footer from './components/Footer';
 
 function App() {
   return (
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <footer>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <p><i className="fa fa-copyright" /> Copyright 2020 by Grad School  
+            | Design: <a href="https://templatemo.com" rel="sponsored" target="_parent">TemplateMo</a></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+    </div>
+  )
+}
+
+export default Footer
