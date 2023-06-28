d1603e020362d97bb42945ca239f250f4c5fdd4c
GilbertMilano10 <s21910363@student.unklab.ac.id>
2023-06-27 23:21:17 -0700

    add section2

--- src/App.js
+++ src/App.js
@@ -2,25 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Section1 from './components/Section1';
+import Section2 from './components/Section2';
 
 function App() {
   return (
     <div>
-  {/* ***** Main Banner Area Start ***** */}
-  <section className="section main-banner" id="top" data-section="section1">
-    <video autoPlay muted loop id="bg-video">
-      <source src="assets/images/course-video.mp4" type="video/mp4" />
-    </video>
-    <div className="video-overlay header-text">
-      <div className="caption">
-        <h6>Graduate School of Management</h6>
-        <h2><em>Your</em> Classroom</h2>
-        <div className="main-button">
-          <div className="scroll-to-section"><a href="#section2">Discover more</a></div>
-        </div>
-      </div>
-    </div>
-  </section>
+  
+ 
   {/* ***** Main Banner Area End ***** */}
   <section className="features">
     <div className="container">
